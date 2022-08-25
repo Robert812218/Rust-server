328b090a10e1d1667bd7af814b33701db4a971a3
Robert812218 <robert812@shaw.ca>
2022-08-25 12:52:50 -0600

    set up project

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+  let string = String::from("127.0.0.1:8080");
+  let string_slice = &string[10..];
+  
+  dbg!(&string);
+  dbg!(string_slice);
+
+  // let server = Server::new("127.0.0.1:8080");
+  // server.run();
+}
+
+struct Server {
+  addr: String,
+}
+
+impl Server {
+  fn new(addr: String) -> Server {
+    Server {
+      addr
+    }
+  }
+
+  fn run(&self) {
+
+  }
+}
