10186591a273db25baadbf66a47826cc7e6aa5d1
Robert812218 <robert812@shaw.ca>
2022-08-26 11:39:36 -0600

    updated server.rs

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,3 @@ fn main() {
   server.run();
 }
 
-/*
-GET /user?id=10 HTTP/1.1\r\n
-HEADERS \r\n
-BODY
-*/
--- src/server.rs
+++ src/server.rs
@@ -1,16 +1,43 @@
-mod server {
-  pub struct Server {
-    addr: String,
+use crate::http:: {ParseError, Request, Response, StatusCode}
+use std::convert::TryFrom;
+use std::io::Read;
+use std::net::TcpListener;
+
+pub struct Server {
+  addr: String,
+}
+
+pub trait Handler {
+  fn handle_request(&mut self, request: &Request) -> Response;
+
+  fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+    println!("Failed to parse request: {}", e);
+    Response::new(StatusCode::BadRequest, None);
   }
-  
-  impl Server {
-    pub fn new(addr: String) -> Server {
-      Self { addr }
-    }
-  
-    pub fn run(self) {
-      println!("Listening on {}", self.addr);
+}
+
+pub struct Server {
+  addr: String;
+}
+
+impl Server {
+  pub fn new(addr: String) -> Self {
+    Self { addr }
+  }
+
+  pub run(self) {
+    println!("Listening on {}", self.addr);
+
+    let listener = TcpListener::bind(&self.addr).unwrap();
+
+    loop {
+      match listener.accept() {
+        Ok((stream, _)) => {
+          let a = 5;
+          println!("OK");
+        }
+        Err(e) => println!("Failed to establish a connection: {}", e);
+      }
     }
   }
-  
 }
