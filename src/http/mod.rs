f51ae084f5a7bb4ddc3ac1c0dc3875bade9bea88
Robert812218 <robert812@shaw.ca>
2022-08-28 11:09:11 -0600

    add errors/error handling

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,5 +1,6 @@
 use http::Request;
 use http::Method;
+pub use request::ParseError;
 
 pub mod request;
 pub mod method;
--- src/http/request.rs
+++ src/http/request.rs
@@ -1,39 +1,74 @@
+use std::str::Utf8Error;
 use std::io::Read;
 use std::convert::TryFrom;
 use std::convert::TryInto;
-use crate::http::Request;
+use crate: :http::Request;
 use std::net::TcpListener;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
-pub struct Server {
-  addr: String,
+
+pub struct Request {
+  path: String,
+  query string: Option<String>,
+  method: Method,
 }
 
-impl Server {
-  pub fn new(addr: String) -> Self {
-    Self { addr }
-  }
+impl TryFrom<&[u8]> for Request {
+  type Error = String;
 
-  pub fn run(self) {
-    println!("Listening on {}", self.addr);
+  // Get /search?name=abc&sort=1 HTTP/1.1
 
-    let listener = TcpListener::bind(&self.addr).unwrap();
 
-    loop {
-      match listener.accept() {
-        Ok((mut stream, _) => {
-          let mut buffer = [0, 1024];
-          match stream.read(&mut buffer) {
-            Ok(_) => {
-              println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-
-              match Request::try_from(&buffer[..]);
-              Ok(request) => {},
-              Err(e) => println!("Failed to parse a request: {}", e);
-            }
-            Err(e) => println!("Failed to read from connection: {}", e);
-          }
-        })
-      }
+  fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    match str::from_utf8(buf) {
+      Ok(request) => {},
+      Err(_) => return Err(ParseError::InvalidEncoding),
     }
+
+    match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+      Ok(request) => {},
+      Err(e) => return Err(e),
+    }
+
+    let request = str::from_utf8(buf)?;
+
+    unimplemented!()
+  }
+}
+
+pub enum ParseError {
+  InvalidRequest,
+  InvalidEncoding,
+  InvalidProtocol,
+  InvalidMethod,
+}
+
+impl ParseError {
+  fn message(&self) -> &str {
+    match self {
+      Self::InvalidRequest => "InvalidRequest",
+      Self::InvalidEncoding => "InvalidEncoding",
+      Self::InvalidProtocol => "InvalidProtocol",
+      Self::InvalidMethod => "InvalidMethod",          
+    }
+  }
+}
+
+impl From<Utf8Error> for ParseError {
+  fn from(_: Utf8Error) -> Self {
+    Self::InalidEncoding
+  }
+}
+
+impl Display for ParseError {
+  fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    write!(f, "{}", self.message());
+  }
+}
+
+impl Debug for ParseError {
+  fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    write!(f, "{}", self.message());
   }
 }
